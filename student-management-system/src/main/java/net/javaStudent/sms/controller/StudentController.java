c0fd198d8e63aef0d644c5cdf5a9e6ed6a5aacee
angel zambrano <angeldzambrano99@gmail.com>
2023-06-06 14:08:54 -0700

    created the student controller for listing all students

--- student-management-system/src/main/java/net/javaStudent/sms/controller/StudentController.java
+++ student-management-system/src/main/java/net/javaStudent/sms/controller/StudentController.java
@@ -1,9 +1,28 @@
 package net.javaStudent.sms.controller;
 
+import net.javaStudent.sms.Service.StudentService;
+import net.javaStudent.sms.dto.StudentDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
+
+    // injectin student service dependency
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     // handler method to handle list of student requests
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return "students";
+    }
 
 }
