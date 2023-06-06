52c4cbfb4aadf6a8326aa6d8c40521a0cd467d32
angel zambrano <angeldzambrano99@gmail.com>
2023-06-06 14:35:04 -0700

    created handler method

--- student-management-system/src/main/java/net/javaStudent/sms/controller/StudentController.java
+++ student-management-system/src/main/java/net/javaStudent/sms/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.javaStudent.sms.controller;
 
 import net.javaStudent.sms.Service.StudentService;
 import net.javaStudent.sms.dto.StudentDto;
+import net.javaStudent.sms.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -23,6 +24,15 @@ public class StudentController {
         List<StudentDto> students = studentService.getAllStudents();
         model.addAttribute("students", students);
         return "students";
+    }
+
+    // handler method to handle new student request
+    @GetMapping("/students/new")
+    public String newStudent(Model model) {
+        // student model object to store student form data
+        StudentDto studentDto  = new StudentDto();
+        model.addAttribute("student", studentDto);
+        return "create_student";
     }
 
 }
