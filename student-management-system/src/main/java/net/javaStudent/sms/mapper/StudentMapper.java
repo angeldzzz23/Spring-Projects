9d1b53558cc2f4da8ada261aceeb9fb867c253f6
angel zambrano <angeldzambrano99@gmail.com>
2023-06-06 13:56:06 -0700

    created a map for student and studentDto

--- student-management-system/src/main/java/net/javaStudent/sms/mapper/StudentMapper.java
+++ student-management-system/src/main/java/net/javaStudent/sms/mapper/StudentMapper.java
@@ -1,4 +1,30 @@
 package net.javaStudent.sms.mapper;
 
+
+import net.javaStudent.sms.dto.StudentDto;
+import net.javaStudent.sms.entity.Student;
+
 public class StudentMapper {
+    public static StudentDto maptToStudentDto(Student student) {
+        StudentDto studentDto = new StudentDto(
+                student.getId(),
+                student.getFirstName(),
+                student.getLastName(),
+                student.getEmail()
+        );
+
+        return  studentDto;
+    }
+
+    public static Student mapToStudent(StudentDto studentDto) {
+        Student student = new Student(
+                studentDto.getId(),
+                studentDto.getFirstName(),
+                studentDto.getLastName(),
+                studentDto.getEmail()
+        );
+        return student;
+    }
+
+
 }
