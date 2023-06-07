da0b15314ccd2d82ce021d99ae75db2960e948ae
angel zambrano <angeldzambrano99@gmail.com>
2023-06-07 13:28:27 -0700

    added the ui for register

--- registration-login-system/src/main/java/net/javaguides/springboot/controller/AuthController.java
+++ registration-login-system/src/main/java/net/javaguides/springboot/controller/AuthController.java
@@ -1,14 +1,33 @@
 package net.javaguides.springboot.controller;
 
+
+import net.javaguides.springboot.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
     // handle method to handle home page request
+    // handler method to handle home page request
     @GetMapping("/index")
-    public String home() {
+    public String home(){
         return "index";
+    }
+
+    // handler method to handle login request
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    // handler method to handle user registration form request
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        // create model object to store form data
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
 }
---
+++ registration-login-system/src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+    @NotEmpty(message = "Password should be empty")
+    private String password;
+}
