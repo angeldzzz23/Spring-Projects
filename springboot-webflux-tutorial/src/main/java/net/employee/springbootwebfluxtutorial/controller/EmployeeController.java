ca7a5a3be43996f2433cc8b5bc21968aa7087d5f
angel zambrano <angeldzambrano99@gmail.com>
2023-06-08 15:03:29 -0700

    added the employee controller

--- springboot-webflux-tutorial/src/main/java/net/employee/springbootwebfluxtutorial/controller/EmployeeController.java
+++ springboot-webflux-tutorial/src/main/java/net/employee/springbootwebfluxtutorial/controller/EmployeeController.java
@@ -1,9 +1,25 @@
 package net.employee.springbootwebfluxtutorial.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import net.employee.springbootwebfluxtutorial.dto.EmployeeDto;
+import net.employee.springbootwebfluxtutorial.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/employees")
 public class EmployeeController {
+    private EmployeeService employeeService;
+
+    // Build reactive save employee REST API
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<EmployeeDto> savedEmployee(@RequestBody EmployeeDto employeeDto) {
+        return employeeService.saveEmployee(employeeDto);
+    }
+
+
+
 }
