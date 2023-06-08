8468d76f776f09f52b792c6a69c52ea0ee23496a
angel zambrano <angeldzambrano99@gmail.com>
2023-06-08 14:15:00 -0700

    added project

---
+++ springboot-webflux-tutorial/src/main/java/net/employee/springbootwebfluxtutorial/SpringbootWebfluxTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.employee.springbootwebfluxtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxTutorialApplication.class, args);
+	}
+
+}
