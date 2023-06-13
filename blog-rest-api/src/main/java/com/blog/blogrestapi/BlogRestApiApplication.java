97ce0619fb895a842389aec410095020e59ea7a9
angel zambrano <angeldzambrano99@gmail.com>
2023-06-13 12:15:50 -0700

    added project

---
+++ blog-rest-api/src/main/java/com/blog/blogrestapi/BlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestApiApplication.class, args);
+	}
+
+}
---
+++ blog-rest-api/src/test/java/com/blog/blogrestapi/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
