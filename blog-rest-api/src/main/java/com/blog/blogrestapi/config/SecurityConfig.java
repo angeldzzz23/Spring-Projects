4fb452bbcd0dfcac063ce0cc8fe72fdbd635dd83
angel zambrano <angeldzambrano99@gmail.com>
2023-06-17 10:27:51 -0700

    added the customUserDetailService

--- blog-rest-api/src/main/java/com/blog/blogrestapi/config/SecurityConfig.java
+++ blog-rest-api/src/main/java/com/blog/blogrestapi/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.blog.blogrestapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +20,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -35,22 +49,22 @@ public class SecurityConfig {
     }
 
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails angel = User.builder()
-                .username("angel")
-                .password(passwordEncoder().encode("Sheep787"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(angel, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails angel = User.builder()
+//                .username("angel")
+//                .password(passwordEncoder().encode("Sheep787"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(angel, admin);
+//    }
 
 
 
