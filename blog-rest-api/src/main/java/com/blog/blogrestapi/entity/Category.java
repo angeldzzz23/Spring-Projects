fdfa9b83f82a213b77782b3406d2d193ce7980fe
angel zambrano <angeldzambrano99@gmail.com>
2023-06-20 14:54:49 -0700

    created category dto

---
+++ blog-rest-api/src/main/java/com/blog/blogrestapi/entity/Category.java
@@ -0,0 +1,29 @@
+package com.blog.blogrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+
+
+}
