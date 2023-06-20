d1d9ab04b4c812c8b9b595bd6e703076daa051fa
angel zambrano <angeldzambrano99@gmail.com>
2023-06-20 12:50:46 -0700

    jetTokeProvider

---
+++ blog-rest-api/src/main/java/com/blog/blogrestapi/security/JwtTokenProvider.java
@@ -0,0 +1,84 @@
+package com.blog.blogrestapi.security;
+
+import com.blog.blogrestapi.exception.BlogAPIException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app-jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    // generate jwt token
+    public String generateToken(Authentication authentication) {
+        String username = authentication.name();
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+    // get username fromm jwt token
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        String username = claims.getSubject();
+
+        return username;
+    }
+
+    // validate Jet token
+    public boolean validateToken(String token) {
+
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (MalformedJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JTW claims string is empty");
+        }
+
+
+    }
+
+
+
+}
