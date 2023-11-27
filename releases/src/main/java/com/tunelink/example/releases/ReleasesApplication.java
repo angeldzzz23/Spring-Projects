b5a5fad57152300c63a0a2ba18d518f4fe9c3145
angel zambrano <angeldzambrano99@gmail.com>
2023-11-27 13:36:57 -0800

    added releases project

---
+++ releases/src/main/java/com/tunelink/example/releases/ReleasesApplication.java
@@ -0,0 +1,13 @@
+package com.tunelink.example.releases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasesApplication.class, args);
+	}
+
+}
---
+++ releases/src/test/java/com/tunelink/example/releases/ReleasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunelink.example.releases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
